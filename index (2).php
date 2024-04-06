c012abf995005c7017d6f33e83ff3241a117ca27
bhumikatinker <166208026+bhumikatinker@users.noreply.github.com>
2024-04-06 13:31:16 +0530

    added index( 2).php

---
+++ index (2).php
@@ -0,0 +1,246 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>portofolie</title>
+</head>
+<style>
+    body{
+        background-color:white;
+        color:white;
+    }
+    span{
+        color:green;
+    }
+
+    #d1{
+        float:left;
+        height:80%;
+        width:50%;
+        position:absolute;
+        background-color:black;
+    }
+    #d2{
+        float:left;
+        height:71%;
+        width:50%;
+        position:absolute;
+        left:50%;
+        background-color:black;
+    }
+    #b1{
+        height:7%;
+        width:30%;
+        border:2px solid;
+        border-radius:5px;
+        background-color:black;
+        color:#00563B;
+        border-color:#00563B;
+    }
+    #b1:hover
+    {
+        background-color:#00563B;
+        color:white;
+    }
+    #i1{
+        position:absolute;
+        top:20%;
+        left:20%;
+    }
+    #i1:hover{
+        opacity: 0.6;
+    }
+
+
+    *{
+        margin:0;
+        padding:0;
+    }
+    .navbar{
+        background-color:black;
+        height: 50px;
+        width:100%;
+        margin:3px;
+    }
+.navbar ul{
+    list-style-type:none;
+}
+.navbar ul li{
+    float:left;
+    width:130px;
+}
+.navbar ul li a{
+    color:white;
+    text-decoration:none;
+    line-height:50px;
+    display:block;
+}
+
+.navbar ul li a:hover{
+    color:#00563B;
+ 
+}
+
+#b2{
+        height:40px;
+        width:150px;
+        border:2px solid;
+        border-radius:5px;
+        background-color:black;
+        color:white;
+        border-color:#00563B;
+    }
+#b2:hover
+    {
+        background-color:#00563B;
+    }
+
+    #d4{
+        float:left;
+        height:70%;
+        width:50%;
+        position:absolute;
+        top:80%;
+  
+        background-color:white;
+    }
+    #d5{
+        float:left;
+        height:70%;
+        width:50%;
+        position:absolute;
+        top:80%;
+        left:50%;
+        background-color:white;
+    }
+
+    #i1:hover{
+        opacity: 0.6;
+    }
+
+    #i2{
+        border-radius:90px;
+        margin:10px;
+    }
+   .top{
+    background-color:black;
+   }
+   
+   #i2:hover{
+    animation:blink;
+    }
+    #d8{
+        float:left;
+        height:80%;
+        width:100%;
+        position:absolute;
+        top:1000px;
+        background-color:black;
+        
+    }
+
+    #d9{
+        float:left;
+        height:40%;
+        width:30%;
+        position:absolute;
+        top:190px;
+        left:9%;
+        background-color:#00563B;
+        color:white;
+
+    }
+
+    #d10{
+       
+        height:40%;
+        width:30%;
+        position:absolute;
+        left:60%;
+        top:190px;
+        background-color:#00563B;
+        color:white;
+    
+    }
+
+
+    </style>
+<body>
+    <div class="top">
+<nav class="navbar">
+<ul>
+    <center><li><a href="#"><img src="/contact.jpg" height="1.5%" width="25px" ></a></li></center>
+    <li><a href="#"> Home </a></li>
+    <li><a href="#">  About Us </a></li>
+    <li> <a href="#"> Contract Us </a> </li>
+    <li> <a href="#"> Servise </a></li>
+    <li><a href="#">  More </a></li>
+    <li><a href="#"> <button id ="b2">Contract</button></a></li>
+</ul>
+</nav>
+    <div id="d1">
+<img id="i1" src="/images.jpg" alt ="contract" height="50%" width="290px" >
+
+</div>
+<div id="d2">
+<br><br><br><br><br><br>
+<font size="4" color="#00563B"><h1>Bhumika Tinker</h1></font>
+<font size="4" color="white"><h1>I'm a Web </h1></font>
+    <font size="7" color="#00563B"> Developer</font>
+    <font size="4" color="white"><p>Lorem ipsm dolor sit amet consectetur adipisicing elit.<br>
+    Doloremque illum nam nobis,minima laudantium fugit sequi <br>nostrm quod 
+    impedit beatae necessitatibus praesentium optio labore nemo!</p><br><br></font>
+    <button id ="b1">Contract</button>
+</div>
+
+</div>
+    <br> <br><br>
+
+
+    <div id="d4">
+<img id="i1" src="/1712247492867.jpg" alt ="contract" height="70%" width="240px" >
+
+</div>
+<div id="d5">
+<br><br><br><br><br><br><font size="5" color="#00563B"><h1>About Me</h1></font><br>
+<font size="3" color="black"><h1>Hello! I'm Bhumika Tinker</h1></font><br>
+    <font size="4" color="black"><p>Lorem ipsm dolor sit amet consectetur adipisicing elit.<br>
+    Doloremque illum nam nobis,minima laudantium fugit sequi <br>nostrm quod 
+    impedit beatae necessitatibus praesentium optio labore nemo!</p><br><br></font>
+    <a href="#"><img id="i2" src="/facebook.png" alt ="contract" height="50px" width="50px" ></a>
+    <a href="#"><img id="i2" src="/insta.jpg" alt ="contract" height="50px" width="50px"></a>
+    <a href="#"><img id="i2" src="/you tube.png" alt ="contract" height="50px" width="50px" ></a>
+
+</div>
+    <br> <br><br>
+
+
+
+<div id="d8">
+<br><br><br>
+<center><font size="5" color="#00563B"><h1>Services</h1></font>
+<font size="2" color="white"><h1>All Your Digital Needs.</h1></font></center>
+<div id="d9">
+    <center><font size="4" color="black"><h1>Web Design</h1></font>
+    <font size="4" color="white"><p>Lorem ipsm dolor sit amet consectetur adipisicing elit.<br>
+    Doloremque illum nam nobis,minima laudantium fugit sequi <br>nostrm quod 
+    impedit beatae necessitatibus praesentium optio labore nemo!</p><br><br></font>
+
+</div>
+<div id="d10">
+<center><font size="4" color="black"><h1>Web Development</h1></font>
+    <font size="4" color="white"><p>Lorem ipsm dolor sit amet consectetur adipisicing elit.<br>
+    Doloremque illum nam nobis,minima laudantium fugit sequi <br>nostrm quod 
+    impedit beatae necessitatibus praesentium optio labore nemo!</p><br><br></font>
+
+</div>
+
+
+
+</div>
+
+
+    
+</body>
+</html>
